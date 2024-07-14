88677393336a62accbfb9b9f469c9b33936796d0
vikashkumar6207 <vktd828403@gmail.com>
2024-07-14 07:22:15 +0530

    don

--- src/components/useToggle.js
+++ src/components/useToggle.js
@@ -1,7 +1,9 @@
 import React,{useCallback,useState} from 'react'
 
 const useToggle = () => {
-   
+  const [state, setState] = useState(initialState);
+  const toggle = useCallback(()=>setState(state => !state), []);
+  return [state, toggle]
   
   }
   export default useToggle;
